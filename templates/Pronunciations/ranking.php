8b63375631fd594f53f0c5f11d4622574dfbdec4
Josh <joshv2@gmail.com>
2021-06-23 22:11:41 +0300

    Fix small error in p mangage

--- templates/Pronunciations/ranking.php
+++ templates/Pronunciations/ranking.php
@@ -73,7 +73,7 @@
                                                     $audioPlayer, 
                                                     $p->pronunciation, 
                                                     status_to_words($p->approved),
-                                                    $p->user->username,
+                                                    (!empty($p->user->username)) ? $p->user->username : '',
                                                     $this->Form->hidden('pronunciations.' . $i . '.id', ['value' => $p->id]), 
                                                     $this->Form->postLink(
                                                         '<i class="icon-trash"></i> Delete',
