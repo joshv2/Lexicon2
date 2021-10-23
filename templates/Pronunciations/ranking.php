2d2a217700b6bb64a72d5aad2bb017ac555087a5
Josh <joshv2@gmail.com>
2021-10-23 21:24:50 +0300

    Fix fail on manage ranking page

--- templates/Pronunciations/ranking.php
+++ templates/Pronunciations/ranking.php
@@ -19,7 +19,7 @@
 	<?php echo $this->element('user_bar');?>
 </nav>
 <div class="column-responsive column-80">
-<h2><?php echo 'Order pronunciations for ' . $this->Html->link(__($word->spelling), ['controller' => 'Words', 'action' => 'view', $word->id]); ?></h2>
+<h2><?php echo 'Order pronunciations for ' . $this->Html->link($word->spelling, ['controller' => 'Words', 'action' => 'view', $word->id]); ?></h2>
     <div class="pronunciations form content">
         <p><?=__("Pronunciations are shown in their current ranking. Ranking is for approved pronunciations only.")?></p>
         <?= $this->Form->create() ?>
@@ -41,8 +41,8 @@
                 <?php echo $this->Html->tableCells([[$p->spelling, 
                                                     $audioPlayer, 
                                                     $p->pronunciation, 
-                                                    $this->Form->hidden(__('pronunciations.' . $i . '.id', ['value' => $p->id]), 
-                                                    $this->Form->control(__('pronunciations.' . $i . '.display_order'), ['label' => false])
+                                                    $this->Form->hidden('pronunciations.' . $i . '.id', ['value' => $p->id]), 
+                                                    $this->Form->control('pronunciations.' . $i . '.display_order', ['label' => false])
                                                    ]]); 
                         $i += 1; ?>
                 <?php endif; ?>
@@ -74,15 +74,15 @@
                                                     $p->pronunciation, 
                                                     status_to_words($p->approved),
                                                     (!empty($p->submitting_user->username)) ? $p->submitting_user->username : 'Submitted by Public',
-                                                    $this->Form->hidden(__('pronunciations.' . $i . '.id')), ['value' => $p->id]), 
-                                                    $this->Form->postLink(__(
-                                                        '<i class="icon-trash"></i> Delete'),
+                                                    $this->Form->hidden('pronunciations.' . $i . '.id', ['value' => $p->id]), 
+                                                    $this->Form->postLink(
+                                                        '<i class="icon-trash"></i> Delete',
                                                         ['prefix' => false, 'controller' => 'Pronunciations', 'action' => 'delete', $p->id, $word->id], 
                                                         ['confirm' => 'Are you sure you want to delete this pronunciation?', 'escape' => false, 'class' => 'button red']),
-                                                    $this->Html->link(__('<i class="fas fa-times"></i> Deny'),
+                                                    $this->Html->link('<i class="fas fa-times"></i> Deny',
                                                     ['prefix' => false, 'controller' => 'Pronunciations', 'action' => 'edit', $p->id, $word->id], ['escape' => false, 'class' => 'button orange']),
-                                                    $this->Form->postLink(__(
-                                                        '<i class="fas fa-check"></i> Approve'),
+                                                    $this->Form->postLink(
+                                                        '<i class="fas fa-check"></i> Approve',
                                                         ['prefix' => false, 'controller' => 'Pronunciations', 'action' => 'approve', $p->id, $word->id], 
                                                         ['confirm' => 'Are you sure you want to approve this pronunciation?', 'escape' => false, 'class' => 'button green'])
                                                    ]]); 
