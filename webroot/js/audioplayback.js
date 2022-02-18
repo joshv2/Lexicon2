2e003d6348277e7516e10b8535f50ed4729aafda
Josh <joshv2@gmail.com>
2022-02-18 08:55:13 +0200

    Fix audio payback with new logo

--- webroot/js/audioplayback.js
+++ webroot/js/audioplayback.js
@@ -1,27 +1,23 @@
-$(function()
-{
-$('[id^=play-pause-button-]').on("click",function(){
-   //alert(this.id);
-    var curelementsplit = this.id.split("-");
-   var playernumber = curelementsplit[3];
-   if($(this).hasClass('fa-play'))
-   {
-     $(this).removeClass('fa-play');
-     $(this).addClass('fa-pause');
-     $("#audioplayer" + playernumber).get(0).play();
-   }
-  else
-   {
-     $(this).removeClass('fa-pause');
-     $(this).addClass('fa-play');
-     $("#audioplayer" + playernumber).get(0).pause();
-   }
+$(function() {
+    $('[id^=play-pause-button-]').on("click", function() {
+        //alert(this.id);
+        var curelementsplit = this.id.split("-");
+        var playernumber = curelementsplit[3];
+        if ($(this).hasClass('fa-volume-up')) {
+            $(this).removeClass('fa-volume-up');
+            $(this).addClass('fa-pause');
+            $("#audioplayer" + playernumber).get(0).play();
+        } else {
+            $(this).removeClass('fa-pause');
+            $(this).addClass('fa-volume-up');
+            $("#audioplayer" + playernumber).get(0).pause();
+        }
 
 
-$("#audioplayer" + playernumber).get(0).onended = function() {
-     $("#play-pause-button-" + playernumber).removeClass('fa-pause');
-     $("#play-pause-button-" + playernumber).addClass('fa-play');
-};
-});
+        $("#audioplayer" + playernumber).get(0).onended = function() {
+            $("#play-pause-button-" + playernumber).removeClass('fa-pause');
+            $("#play-pause-button-" + playernumber).addClass('fa-volume-up');
+        };
+    });
 
 });
