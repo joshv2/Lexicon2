44371d1016890ac41cb892cbe11876be476f3fb8
Josh <joshv2@gmail.com>
2022-03-11 11:11:26 +0200

    test mp3

--- templates/layout/googleads.php
+++ templates/layout/googleads.php
@@ -3,8 +3,10 @@
 <head>
 </head>
 <body>
-    <?= $this->fetch('content') ?>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2080909425204614"
-     crossorigin="anonymous"></script>
+    
+<?php 
+ echo '<a id="play-pause-button-0" class="fa fa-volume-up"> <span id="listen">listen</span></a><video controls="controls" class="audioplayers" id="audioplayer0" src="/recordings/adar16244732060.mp3"></video>';
+
+					 ?>
 </body>
 </html>
