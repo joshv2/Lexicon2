4c21577e99568263a4e881eb19b7431f1d486350
Josh <joshv2@gmail.com>
2023-09-05 20:57:19 +0300

    Fix JS for FontAwesome

--- webroot/js/audioplayback.js
+++ webroot/js/audioplayback.js
@@ -3,21 +3,21 @@ $(function() {
         //alert(this.id);
         var curelementsplit = this.id.split("-");
         var playernumber = curelementsplit[3];
-        if ($(this).hasClass('fa-volume-up')) {
-            $(this).removeClass('fa-volume-up');
-            $(this).addClass('fa-pause');
+        if ($(this).hasClass('fa-solid fa-volume-up')) {
+            $(this).removeClass('fa-solid fa-volume-up');
+            $(this).addClass('fa-solid fa-pause');
             console.log($("#audioplayer" + playernumber).get(0));
             $("#audioplayer" + playernumber).get(0).play();
         } else {
-            $(this).removeClass('fa-pause');
-            $(this).addClass('fa-volume-up');
+            $(this).removeClass('fa-solid fa-pause');
+            $(this).addClass('fa-solid fa-volume-up');
             $("#audioplayer" + playernumber).get(0).pause();
         }
 
 
         $("#audioplayer" + playernumber).get(0).onended = function() {
-            $("#play-pause-button-" + playernumber).removeClass('fa-pause');
-            $("#play-pause-button-" + playernumber).addClass('fa-volume-up');
+            $("#play-pause-button-" + playernumber).removeClass('fa-solid fa-pause');
+            $("#play-pause-button-" + playernumber).addClass('fa-solid fa-volume-up');
         };
     });
 
