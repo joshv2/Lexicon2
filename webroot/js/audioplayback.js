5196e4a521ec7ebbf355ff230c7add352cca1056
Josh <joshv2@gmail.com>
2023-09-05 21:07:56 +0300

    Attempt #2

--- webroot/js/audioplayback.js
+++ webroot/js/audioplayback.js
@@ -3,21 +3,21 @@ $(function() {
         //alert(this.id);
         var curelementsplit = this.id.split("-");
         var playernumber = curelementsplit[3];
-        if ($(this).children[0].hasClass('fa-solid fa-volume-up')) {
-            $(this).children[0].removeClass('fa-solid fa-volume-up');
-            $(this).children[0].addClass('fa-solid fa-pause');
+        if ($(this).children.eq(0).hasClass('fa-solid fa-volume-up')) {
+            $(this).children.eq(0).removeClass('fa-solid fa-volume-up');
+            $(this).children.eq(0).addClass('fa-solid fa-pause');
             console.log($("#audioplayer" + playernumber).get(0));
             $("#audioplayer" + playernumber).get(0).play();
         } else {
-            $(this).children[0].removeClass('fa-solid fa-pause');
-            $(this).children[0].addClass('fa-solid fa-volume-up');
+            $(this).children.eq(0).removeClass('fa-solid fa-pause');
+            $(this).children.eq(0).addClass('fa-solid fa-volume-up');
             $("#audioplayer" + playernumber).get(0).pause();
         }
 
 
         $("#audioplayer" + playernumber).get(0).onended = function() {
-            $("#play-pause-button-" + playernumber).children[0].removeClass('fa-solid fa-pause');
-            $("#play-pause-button-" + playernumber).children[0].addClass('fa-solid fa-volume-up');
+            $("#play-pause-button-" + playernumber).children.eq(0).removeClass('fa-solid fa-pause');
+            $("#play-pause-button-" + playernumber).children.eq(0).addClass('fa-solid fa-volume-up');
         };
     });
 
