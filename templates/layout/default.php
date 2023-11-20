23b804e732041b126c683abcc814b4475c2fcb51
Josh <joshv2@gmail.com>
2023-11-20 19:41:03 +0200

    Stash for Header and Multiselect

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -23,6 +23,12 @@
 		'as' => 'font']
 	);?>
 	<!--<script src="https://kit.fontawesome.com/3f405633a9.js" crossorigin="anonymous"></script>-->
+	
+	<?php echo $this->Html->css('output.css');?>
+	<!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">-->
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+	
 	<?php echo $this->Html->css('fontawesome');?>
 	<?php echo $this->Html->css('solid');?>
 	<?= $this->Html->meta('csrfToken', $this->request->getAttribute('csrfToken')); ?>
@@ -68,6 +74,29 @@
 
 </head>
 <body>
+
+
+
+  	<div class="row">
+		<div class="col">
+			1 of 3
+		</div>
+		<div class="col-6">
+			<nav class="navbar navbar-expand-lg navbar-dark bg-gradient-dark">
+			<div class="navbar-brand">
+				<?= $this->cell('Logo'); ?>
+			</div>
+			</nav>
+		</div>
+		<div class="col">
+			3 of 3
+		</div>
+	</div>
+
+
+
+
+
 <div id="container">
 	<header>
 	<div id="desktop">	
