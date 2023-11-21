8c13683dca4c8c285902f92a2c6ace4939780d51
Josh <joshv2@gmail.com>
2023-11-21 20:55:32 +0200

    More header work

--- templates/cell/Logo/display.php
+++ templates/cell/Logo/display.php
@@ -1,5 +1,5 @@
 <?php echo $this->Html->image($sitei18n . '/JewishLex_Logo.jpg' , 
-                                            ['url' => '/', 
+                                            [
                                             'class' => "d-inline-block align-top",
                                             'alt'=> "Home Logo",
                                             'height' => 51, 
--- templates/layout/default.php
+++ templates/layout/default.php
@@ -83,11 +83,44 @@
 		</div>
 		<div class="col-6">
 			<nav class="navbar navbar-expand-lg navbar-dark bg-gradient-dark">
-			<div class="navbar-brand">
+			<a class="navbar-brand">
 				<?= $this->cell('Logo'); ?>
-			</div>
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto mx-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Add a Word <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="#">About Us <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Notes <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Jewish Languages <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Volunteer <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Donate <span class="sr-only">(current)</span></a>
+					</li>
+				</ul>
+
+			<form class="form-inline">
+				<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+			</form>
+		</div> <!-- nav -->
 			</nav>
-		</div>
+		</div> <!--column-->
 		<div class="col">
 			3 of 3
 		</div>
