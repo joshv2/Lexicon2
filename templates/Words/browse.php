016b034ab03c43333799c0a523ec0e21f5d225f4
Josh <joshv2@gmail.com>
2023-12-27 20:41:34 +0200

    Fix Mobile display on browse

--- templates/Words/browse.php
+++ templates/Words/browse.php
@@ -6,6 +6,43 @@
 										['class' => 'button blue', 'escape' => false]);?></li>
 	</ul>
 </nav>
+<div class="checkbox-container">
+  <div class="category">
+    <h3 class="category-header">Category 1</h3>
+    <div class="checkboxes">
+      <input type="checkbox" id="checkbox1-1" name="checkbox1-1">
+      <label for="checkbox1-1">Checkbox 1.1</label><br>
+      <!-- Add more checkboxes for Category 1 as needed -->
+    </div>
+  </div>
+
+  <div class="category">
+    <h3 class="category-header">Category 2</h3>
+    <div class="checkboxes">
+      <input type="checkbox" id="checkbox2-1" name="checkbox2-1">
+      <label for="checkbox2-1">Checkbox 2.1</label><br>
+      <!-- Add more checkboxes for Category 2 as needed -->
+    </div>
+  </div>
+
+  <div class="category">
+    <h3 class="category-header">Category 3</h3>
+    <div class="checkboxes">
+      <input type="checkbox" id="checkbox3-1" name="checkbox3-1">
+      <label for="checkbox3-1">Checkbox 3.1</label><br>
+      <!-- Add more checkboxes for Category 2 as needed -->
+    </div>
+  </div>
+  <!-- Repeat the above structure for Categories 3 and 4 -->
+  <div class="category">
+    <h3 class="category-header">Category 4</h3>
+    <div class="checkboxes">
+      <input type="checkbox" id="checkbox4-1" name="checkbox4-1">
+      <label for="checkbox4-1">Checkbox 4.1</label><br>
+      <!-- Add more checkboxes for Category 2 as needed -->
+    </div>
+  </div>
+</div>
 
 <section id="main">
 	<nav id="mobilefilterbrowse">
@@ -47,12 +84,12 @@
 			<li class="main">
 				<div class="dropdown">
 					<a class="main">
-						<span><?php echo empty($current_condition['use']) ? __('All Uses') : '<b>'. __('Use') . ':</b> ' . __($types[$current_condition['use']]);?></span>
+						<span><?php echo empty($current_condition['use']) ? __('All Uses') : '<b>'. __('Use') . ':</b> ' . __($types[$current_condition['use']]['type']);?></span>
 					</a>
 					<div class="submenu">
 						<ul>
 							<li><a href="<?php echo $this->QueryString->remove('use');?>"><?php echo __('All'); ?></a></li>
-						<?php foreach ($types as $k => $o): ?><li><a href="<?php echo $this->QueryString->add('use', $k); ?>"><?php echo __($o);?></a></li><?php endforeach;?>
+						<?php foreach ($types as $k => $o): ?><li><a href="<?php echo $this->QueryString->add('use', $k); ?>"><?php echo __($o['type']);?></a></li><?php endforeach;?>
 						</ul>
 					</div>
 				</div>
