b2026d445145a4408e9b5fc8d950f4f8855f8657
Josh <joshv2@gmail.com>
2024-07-09 20:47:16 +0300

    Tighten Up Search Remove commented lines

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -8,17 +8,15 @@ class SearchController extends AppController {
 	public function initialize(): void
     {
         parent::initialize();
-        //$this->loadComponent('Paginator');
     }
 
 	public function index()
     {
-        //array_map([$this, 'loadModel'], ['Words']);
         $sitelang = $this->viewBuilder()->getVar('sitelang');
         $q = $this->request->getQuery('q');
 
 
-		$words = $this->paginate($this->fetchTable('Words')->find('searchResults', ['querystring' => $q, 'langid' => $sitelang->id]));
+		$words = $this->paginate($this->fetchTable('Words')->find('searchResults', querystring: $q, langid: $sitelang->id));
 
         $this->set(compact('words', 'q'));
         $this->render('results');
